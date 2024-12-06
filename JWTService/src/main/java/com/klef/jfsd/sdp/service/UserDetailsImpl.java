322b9819571e933652a363e02eac4961de87d080
Gaurav-Khivasara <gkgauravgk123@gmail.com>
2024-12-06 22:25:42 +0530

    commented unwanted imports

--- JWTService/src/main/java/com/klef/jfsd/sdp/service/UserDetailsImpl.java
+++ JWTService/src/main/java/com/klef/jfsd/sdp/service/UserDetailsImpl.java
@@ -1,8 +1,8 @@
 package com.klef.jfsd.sdp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.klef.jfsd.sdp.model.User;
