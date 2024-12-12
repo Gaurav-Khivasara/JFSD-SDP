38e27ea0d0cfb6d4af70736f80245068a25d744f
Gaurav-Khivasara <gkgauravgk123@gmail.com>
2024-12-12 09:22:55 +0530

    changes from deployed branch ananta

--- EmailService/src/main/java/com/klef/jfsd/sdp/EmailServiceApplication.java
+++ EmailService/src/main/java/com/klef/jfsd/sdp/EmailServiceApplication.java
@@ -3,14 +3,10 @@ package com.klef.jfsd.sdp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv.load().entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		
 		SpringApplication.run(EmailServiceApplication.class, args);
 		System.out.println("Email Service Running...!");
 	}
